d19d4ee15f5eb156399d737b336fe1917f6663a5
liaozhongqiang <liaozhongqiang2014@163.com>
2015-11-09 17:51:04 +0800

    测试rest/{xx}形式的接口

--- src/main/java/com/my/action/TestAction.java
+++ src/main/java/com/my/action/TestAction.java
@@ -1,6 +1,8 @@
 package com.my.action;
 
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -12,5 +14,17 @@ public class TestAction {
 	@ResponseBody
 	public String sayHello(){
 		return "ฤ๚บรฃก mvc";
+	}
+	
+	@RequestMapping("rest1/{id}")
+	@ResponseBody
+	public String rest1(@PathVariable String id){
+		return id;
+	}
+	
+	@RequestMapping("rest2/{val1}/{val2}")
+	@ResponseBody
+	public String rest2(@PathVariable("val1") String val1,@PathVariable("val2") String val2){
+		return val1+val2;
 	}
 }
